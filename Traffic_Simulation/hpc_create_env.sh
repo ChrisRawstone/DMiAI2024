158b5250ff8c23703cafa1ccf9a312d29d4e41a0
Christian Raasteen <dr.rawstone@outlook.com>
2024-09-27 12:06:45 +0200

    add environments scripts

--- Traffic_Simulation/hpc_create_env.sh
+++ Traffic_Simulation/hpc_create_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # First, log into the A100 node and then execute the following commands
-# a100sh << 'EOF'
+a100sh << 'EOF'
 module load python3/3.10.14
 python3 -m venv traffic_env
 source traffic_env/bin/activate
 pip install -r requirements.txt
-# EOF
+EOF
 
