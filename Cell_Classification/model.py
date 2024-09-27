26ab5c7c4b2b5cb18210aa65d6aca7c85caaffa2
JasminThari <s204155@student.dtu.dk>
2024-09-27 19:10:30 +0200

    updating model for predictions

--- Cell_Classification/model.py
+++ Cell_Classification/model.py
@@ -1,16 +1,39 @@
 import numpy as np
 import random
+import torch
+from torchvision import transforms
+from src.models.model import SimpleClassifier
+from src.data.make_dataset import PadToSize
 
-### CALL YOUR CUSTOM MODEL VIA THIS FUNCTION ###
 def predict(image: np.ndarray) -> int:
 
-    is_homogenous = example_model(image)
+    model = SimpleClassifier(num_classes=2)  
+    model.load_state_dict(torch.load('/Users/jasmink.j.thari/GitHub/DM-i-AI-2024/Cell_Classification/model.pth'))
+    model.eval()
+
+    target_width = 1500
+    target_height = 1470
+    transform = transforms.Compose([
+                                    PadToSize(target_width=target_width, target_height=target_height),  # Custom padding transform
+                                    transforms.Grayscale(num_output_channels=3),
+                                    transforms.ToTensor()])
+    
+    image = transform(image)
+    
+    is_homogenous = model(image)
 
     return is_homogenous
 
 
-def example_model(image) -> int:
+# def example_model(image) -> int:
 
-    is_homogenous = random.randint(0, 1)
+#     is_homogenous = random.randint(0, 1)
 
-    return is_homogenous
+#     return is_homogenous
+
+def test_predict():
+    image = np.random.rand(1470, 1500, 3)
+    assert predict(image) in [0, 1]
+
+test_predict()
+
