c67a104d9805ddf5bd95cbd78e9c0a2858df8f4f
ChrisRawstone <dr.rawstone@outlook.com>
2024-10-03 19:51:24 +0200

    two independent lanes control

--- Traffic_Simulation/sim/run_sim.py
+++ Traffic_Simulation/sim/run_sim.py
@@ -6,43 +6,40 @@ from environment_gui import load_and_run_simulation
 from typing import Dict
 from collections import defaultdict
 
-
-
 #defining global variables
-global time_since_signals_has_been_green, wait_before_start_ticks, metrics
-
+global time_since_signals_has_been_green, wait_before_start_ticks, metrics, previous_green_signals
 
 # Define threshold for stopped vehicles
 STOPPED_SPEED_THRESHOLD = 0.5  # Speed below which a vehicle is considered stopped
 QUEUE_DISTANCE_THRESHOLD = 50.0  # Distance within which to consider vehicles for queue length
 
+previous_green_signals = []
 
-delay = 2
+delay = 0
 wait_before_start_ticks = 15
 time_since_signals_has_been_green = 1000
 
-
 metrics = defaultdict(dict)
-
 
 pairs_of_signals = {
     "A1" : "A2",
     "A2" : "A1",  
-    "A1Left" : "A2Left",
-    "A2Left" : "A1Left",  
+    "A1LeftTurn" : "A2LeftTurn",
+    "A2LeftTurn" : "A1LeftTurn",  
     "B1" : "B2",
     "B2" : "B1",  
-    "B1Left" : "B2Left",
-    "B2Left" : "B1Left"  
+    "B1LeftTurn" : "B2LeftTurn",
+    "B2LeftTurn" : "B1LeftTurn"  
 }
 
 
 corresponding_sidelanes = {
-    "A1" : "A1Left",
-    "A2" : "A2Left",
-    "B1" : "B1Left",
-    "B2" : "B2Left"
+    "A1" : "A1LeftTurn",
+    "A2" : "A2LeftTurn",
+    "B1" : "B1LeftTurn",
+    "B2" : "B2LeftTurn"
 }
+inverse_sidelanes = {v: k for k, v in corresponding_sidelanes.items()}
 
 
 # Helper functions
@@ -76,18 +73,30 @@ def find_two_signals_with_most_waiting_time(state):
 
     sorted_legs = sorted(metrics.items(), key=lambda x: x[1][heuristic], reverse=True)
     signal_with_most_waiting_time = sorted_legs[0][0]
+    
     chosen_combination = None
     for combination in state.allowed_green_signal_combinations:
         if signal_with_most_waiting_time==combination.name:
                 signal_with_2nd_most_waiting_time = max([leg for leg in combination.groups if leg in metrics], key=lambda leg: metrics[leg][heuristic])
                 chosen_combination = (signal_with_most_waiting_time, signal_with_2nd_most_waiting_time)
-    
+
+    # if signal_with_most_waiting_time in corresponding_sidelanes:
+    #     # If signal is in corresponding_sidelanes, get the corresponding sideline
+    #     next_signal = corresponding_sidelanes[signal_with_most_waiting_time]
+    # else:
+    #     next_signal = inverse_sidelanes[signal_with_most_waiting_time]
+
+    # return (signal_with_most_waiting_time, next_signal)
     return chosen_combination
 
+def initialize_metrics(state):
+    global metrics
+    for signal in state.signals:
+        metrics[signal.name] = {
+            'num_stopped': 0,
+        }
 
-
-
-def calculate_leg_metrics(state, leg_waiting_ticks: Dict[str, int]) -> Dict[str, Dict[str, float]]:
+def update_major_metrics(state):
     """
     Calculates various traffic metrics for each leg in the given state.
 
@@ -99,48 +108,20 @@ def calculate_leg_metrics(state, leg_waiting_ticks: Dict[str, int]) -> Dict[str,
         Dict[str, Dict[str, float]]: A dictionary containing the calculated metrics for each leg.
     """
     global metrics
-    
 
-    for leg in state.legs:
+    for leg in state.legs: # A1, A2, B1, B2
         leg_name = leg.name
-        stopped_count = calculate_stopped_vehicles(state.vehicles, leg_name)
-        queue_length = calculate_queue_length(state.vehicles, leg_name)
-        num_vehicles = calculate_amount_vehicle_in_leg(state.vehicles, leg_name)
+        stopped_count_majorlane = calculate_stopped_vehicles(state.vehicles, leg_name)
 
-        # Update waiting ticks
-        if leg_name not in leg_waiting_ticks:
-            leg_waiting_ticks[leg_name] = 0
+        sidelane_signal = corresponding_sidelanes[leg_name]
 
-        if stopped_count > 0:
-            leg_waiting_ticks[leg_name] += stopped_count
-        else:
-            leg_waiting_ticks[leg_name] = 0  # Reset if no vehicles are stopped
-
-        total_wait = leg_waiting_ticks[leg_name]
-        avg_wait = total_wait / stopped_count if stopped_count > 0 else 0
-
+        stopped_count_sidelane = metrics[sidelane_signal]["num_stopped"]
+        
         metrics[leg_name] = {
-            # 'num_vehicles': num_vehicles,
-            'num_stopped': stopped_count,
-            # 'total_waiting_time': total_wait,
-            # 'average_waiting_time': avg_wait,
-            # 'queue_length': queue_length
-        }
-        # metrics[leg_name] = {
-        #     'num_vehicles': num_vehicles,
-        #     'num_stopped': stopped_count,
-        #     'total_waiting_time': total_wait,
-        #     'average_waiting_time': avg_wait,
-        #     'queue_length': queue_length
-        # }
-
-    return leg_waiting_ticks
-
-
+            'num_stopped': (stopped_count_majorlane-stopped_count_sidelane)}
 
 def get_health_sidelane(state, signal):
 
-   
     vehicles_current_leg = get_vehicle_in_specific_leg(state.vehicles, signal)
 
     # Finding the vehicle with the highest distance and speed equal to 0
@@ -162,19 +143,35 @@ def get_health_sidelane(state, signal):
 
     return sidelane_queue_length, num_cars_in_sidelane, proportion_in_speed, proportion_not_in_speed, 
 
-
 def turn_signal_green(prediction, signal):
     prediction["signals"].append({"name": signal, "state": "green"})
     return prediction
 
+def update_minor_metrics(state, current_green_signals):
+    for green_signal in current_green_signals: # remember this only works with major green signals, fix this
+        
+        if len(get_vehicle_in_specific_leg(state.vehicles, green_signal)) > 0:
+            sidelane_queue_length, num_cars_in_sidelane, proportion_in_speed, proportion_not_in_speed = get_health_sidelane(state, green_signal)
+            num_cars_in_major_lane = metrics[green_signal]["num_stopped"]
 
 
+            sidelane_signal = corresponding_sidelanes[green_signal]
+
+            metrics[sidelane_signal]["num_stopped"] = num_cars_in_sidelane
+            metrics[green_signal]["num_stopped"] = num_cars_in_major_lane-num_cars_in_sidelane
 
 
+def has_green_signals_changed(state):
+    global previous_green_signals
+    if get_green_signals(state.signals) != previous_green_signals:
+        previous_green_signals = get_green_signals(state.signals)
+        return True
+    else:
+        return False
 
 
 def run_game():
-    global time_since_signals_has_been_green, wait_before_start_ticks, metrics
+    global time_since_signals_has_been_green, wait_before_start_ticks, metrics, previous_green_signals
 
     test_duration_seconds = 600
     random = True
@@ -193,24 +190,17 @@ def run_game():
         random,
         input_queue,
         output_queue,
-        error_queue
-    ))
+        error_queue))
     
     p.start()
     sleep(0.2)  # Wait for the simulation to start
 
     actions = {}
-    leg_waiting_ticks = {}
-
-
-    
-    
 
     while True:
         state = output_queue.get()
+
         unique_signals = get_unique_signals(state)
-
-
 
         if state.is_terminated:
             p.join()
@@ -218,19 +208,24 @@ def run_game():
 
         current_tick = state.simulation_ticks
 
+        if current_tick <= 10:
+            initialize_metrics(state)
+
         print("############################")
         print("Current tick: ", current_tick)
         print("############################")
 
-        leg_waiting_ticks = calculate_leg_metrics(state, leg_waiting_ticks)
         chosen_combination = None
 
         if current_tick > wait_before_start_ticks:
             if (time_since_signals_has_been_green-delay) > 6:
-
-                time_since_signals_has_been_green = 0
                 
+                update_major_metrics(state)
+                
+                time_since_signals_has_been_green = 0
+            
                 chosen_combination = find_two_signals_with_most_waiting_time(state) # A1, A2, B1, B2
+            
 
         # If a valid combination is found, activate it
         prediction = {"signals": []}
@@ -246,19 +241,11 @@ def run_game():
 
         print(metrics)
 
+
         current_green_signals = get_green_signals(state.signals)
         
-        for green_signal in current_green_signals: # remember this only works with major green signals, fix this
-            
-            if len(get_vehicle_in_specific_leg(state.vehicles, green_signal)) > 0:
-                sidelane_queue_length, num_cars_in_sidelane, proportion_in_speed, proportion_not_in_speed = get_health_sidelane(state, green_signal)
-                num_cars_in_major_lane = metrics[green_signal]["num_stopped"]
-
-
-                sidelane_signal = corresponding_sidelanes[green_signal]
-
-                metrics[sidelane_signal]["num_stopped"] = num_cars_in_sidelane
-                metrics[green_signal]["num_stopped"] = num_cars_in_major_lane-num_cars_in_sidelane
+        if has_green_signals_changed(state):
+            update_minor_metrics(state, current_green_signals)
 
 
 
